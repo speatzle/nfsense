4e1e868054d4f8111c54e3bebfd9604d5754da7c
Samuel Lorch <sam@soontm.de>
2023-03-01 18:18:27 +0100

    add Addresses and Service to Config

--- pkg/definitions/config.go
+++ pkg/definitions/config.go
@@ -3,4 +3,7 @@ package definitions
 type Config struct {
 	ConfigVersion int64     `json:"config_version"`
 	Netfilter     Netfilter `json:"netfilter"`
+	Netfilter     Netfilter          `json:"netfilter"`
+	Addresses     map[string]Address `json:"addresses"`
+	Services      map[string]Service `json:"services"`
 }

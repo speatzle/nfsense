7cf945d4abd4576300f5b881eb4135c15e538666
Samuel Lorch <sam@soontm.de>
2023-03-05 20:18:08 +0100

    remove request test meta field

--- pkg/jsonrpc/request.go
+++ pkg/jsonrpc/request.go
@@ -3,9 +3,8 @@ package jsonrpc
 import "encoding/json"
 
 type request struct {
-	Jsonrpc string            `json:"jsonrpc"`
-	ID      any               `json:"id,omitempty"`
-	Method  string            `json:"method"`
-	Params  json.RawMessage   `json:"params"`
-	Meta    map[string]string `json:"meta,omitempty"`
+	Jsonrpc string          `json:"jsonrpc"`
+	ID      any             `json:"id,omitempty"`
+	Method  string          `json:"method"`
+	Params  json.RawMessage `json:"params"`
 }

f935fac5237afed77728a79679cfe808b7d92216
Samuel Lorch <sam@soontm.de>
2023-03-06 00:02:38 +0100

    Add GetServices API Method

---
+++ api/firewall/services.go
@@ -0,0 +1,20 @@
+package firewall
+
+import (
+	"context"
+
+	"nfsense.net/nfsense/pkg/definitions"
+)
+
+type GetServicesParameters struct {
+}
+
+type GetServicesResult struct {
+	Services map[string]definitions.Service
+}
+
+func (f *Firewall) GetServices(ctx context.Context, params GetForwardRulesParameters) (GetServicesResult, error) {
+	return GetServicesResult{
+		Services: f.Conf.Firewall.Services,
+	}, nil
+}

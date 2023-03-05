126213e8290af839e0c9f71a56e8973353f97fef
Samuel Lorch <sam@soontm.de>
2023-03-05 23:56:18 +0100

    rename netfilter to firewall

--- pkg/definitions/firewall.go
+++ pkg/definitions/firewall.go
@@ -4,4 +4,6 @@ type Firewall struct {
 	ForwardRules        []ForwardRule        `json:"forward_rules"`
 	DestinationNATRules []DestinationNATRule `json:"destination_nat_rules"`
 	SourceNATRules      []SourceNATRule      `json:"source_nat_rules"`
+	Addresses           map[string]Address   `json:"addresses"`
+	Services            map[string]Service   `json:"services"`
 }

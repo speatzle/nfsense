081aeec142024b66f418b2eedbfa13fc50390268
Samuel Lorch <sam@soontm.de>
2023-04-01 18:17:08 +0200

    Add Config API Calls

---
+++ internal/api/config/pending.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/r3labs/diff/v3"
+)
+
+type GetPendingStatusResult struct {
+	Changed bool
+}
+
+func (c *Config) GetPendingStatus(ctx context.Context, params struct{}) (GetPendingStatusResult, error) {
+	return GetPendingStatusResult{
+		Changed: c.ConfigManager.AreChangesPending(),
+	}, nil
+}
+
+type GetPendingChangelogResult struct {
+	Changelog diff.Changelog
+}
+
+func (c *Config) GetPendingChangelog(ctx context.Context, params struct{}) (GetPendingChangelogResult, error) {
+	log, err := c.ConfigManager.GetPendingChangelog()
+	if err != nil {
+		return GetPendingChangelogResult{}, fmt.Errorf("Get Pending changelog %w", err)
+	}
+	return GetPendingChangelogResult{
+		Changelog: log,
+	}, nil
+}

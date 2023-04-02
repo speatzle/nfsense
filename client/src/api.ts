7d895039bf5b3183bd4fc4baf425d177db1da9da
Samuel Lorch <sam@soontm.de>
2023-04-02 13:06:29 +0200

    Add API Error Toast

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,9 @@
 // import WebSocketServer  from 'ws';
 import JsonRPC from 'simple-jsonrpc-js';
 import axios from "axios";
+import { useToast } from 'vue-toast-notification';
+
+const $toast = useToast();
 
 let jrpc = new JsonRPC.connect_xhr('/api');
 // let socket = new WebSocket("ws://"+ window.location.host +"/ws/api");
@@ -21,6 +24,7 @@ export async function apiCall(method: string, params: Record<string, any>): Prom
     if (ex.code === 401) {
       UnauthorizedCallback();
     } else {
+      $toast.error(method+ ': ' + ex.message);
       console.debug("api call epic fail", ex);
     }
     return { Data: null, Error: ex};
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,11 +3,13 @@ import './global-styles/components.css';
 import './global-styles/colors.css';
 import './global-styles/mlfe.css';
 import './global-styles/transitions.css';
+import 'vue-toast-notification/dist/theme-default.css';
 
 import App from './App.vue';
 import { createHead } from '@vueuse/head';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from '~pages';
+import ToastPlugin from 'vue-toast-notification';
 
 const app = createApp(App);
 const head = createHead();
@@ -18,5 +20,6 @@ const router = createRouter({
 
 app.use(router);
 app.use(head);
+app.use(ToastPlugin);
 
 app.mount('#app');

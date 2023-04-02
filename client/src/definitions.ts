2c02451da760d4a1a1e6600fe9c9cdbe5474f0c6
Samuel Lorch <sam@soontm.de>
2023-04-02 19:21:54 +0200

    Add All Interface fields

--- client/src/definitions.ts
+++ client/src/definitions.ts
@@ -1,5 +1,6 @@
 import PillBar from "./components/inputs/PillBar.vue";
 import TextBox from "./components/inputs/TextBox.vue";
+import NumberBox from "./components/inputs/NumberBox.vue";
 import MultilineTextBox from "./components/inputs/MultilineTextBox.vue";
 import CheckBox from "./components/inputs/CheckBox.vue";
 
@@ -20,6 +21,13 @@ export const editTypes: { [key: string]: {[key: string]: any} } = {
         title: "Interfaces",
         fields: [
           {key: "name", label: "Name", component: () => TextBox },
+          {key: "type", label: "Type", component: () => PillBar, props: {options: [{name: 'Hardware', selected: true}, {name: 'VLAN'}, {name: 'Bond'}, {name: 'Brdige'}]}},
+          {key: "hardware_interface", label: "Hardware Interface", component: () => TextBox },
+          {key: "vlan_id", label: "VLAN ID", component: () => NumberBox, props: {min: 1, max: 4094} },
+          {key: "bond_members", label: "Nond Members", component: () => TextBox },
+          {key: "bridge_members", label: "Bridge Memebers", component: () => TextBox },
+          {key: "addressing_mode", label: "Addressing Mode", component: () => PillBar, props: {options: [{name: 'None', selected: true}, {name: 'Static'}, {name: 'DHCP'}]}},
+          {key: "address", label: "Address", component: () => TextBox },
           {key: "comment", label: "Comment", component: () => MultilineTextBox },
         ],
       }

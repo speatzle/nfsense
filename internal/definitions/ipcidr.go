d9ecfae454d036dff8e95f07c7c89802cc8db2c0
Samuel Lorch <sam@soontm.de>
2023-04-08 01:36:20 +0200

    Fix Address Type

---
+++ internal/definitions/ipcidr.go
@@ -0,0 +1,32 @@
+package definitions
+
+import (
+	"encoding/json"
+	"net"
+)
+
+// IPCIDR is IP Address with the mask in CIDR format
+type IPCIDR struct {
+	net.IPNet
+}
+
+// MarshalJSON for IPCIDR
+func (i IPCIDR) MarshalJSON() ([]byte, error) {
+	return json.Marshal(i.String())
+}
+
+// UnmarshalJSON for IPCIDR
+func (i *IPCIDR) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	ip, ipnet, err := net.ParseCIDR(s)
+	if err != nil {
+		return err
+	}
+	i.IPNet = *ipnet
+	i.IPNet.IP = ip
+	return nil
+}

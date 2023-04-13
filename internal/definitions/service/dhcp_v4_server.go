fb49d32ca98d06a67e0df653024624d5efe0360b
Samuel Lorch <sam@soontm.de>
2023-04-13 18:39:24 +0200

    Add DHCP Json tags

--- internal/definitions/service/dhcp_v4_server.go
+++ internal/definitions/service/dhcp_v4_server.go
@@ -5,17 +5,19 @@ import (
 )
 
 type DHCPv4Server struct {
-	Interface        string
-	Pool             []string
-	DefaultLeaseTime time.Duration
-	MaxLeaseTime     time.Duration
+	Interface        string        `json:"interface"`
+	Pool             []string      `json:"pool"`
+	DefaultLeaseTime time.Duration `json:"default_lease_time"`
+	MaxLeaseTime     time.Duration `json:"max_lease_time"`
 
-	GatewayMode   Mode
-	Gateway       *string
-	DNSServerMode Mode
-	DNSServer     *[]string
-	NTPServerMode Mode
-	NTPServer     *[]string
+	GatewayMode   Mode      `json:"gateway_mode"`
+	Gateway       *string   `json:"gateway,omitempty"`
+	DNSServerMode Mode      `json:"dns_server_mode"`
+	DNSServers    *[]string `json:"dns_servers,omitempty"`
+	NTPServerMode Mode      `json:"ntp_server_mode"`
+	NTPServers    *[]string `json:"ntp_servers,omitempty"`
 
-	Reservations []Reservation
+	Reservations []Reservation `json:"reservations,omitempty"`
+
+	Comment string `json:"comment,omitempty"`
 }
--- internal/definitions/service/dhcp_v6_server.go
+++ internal/definitions/service/dhcp_v6_server.go
@@ -3,17 +3,19 @@ package service
 import "time"
 
 type DHCPv6Server struct {
-	Interface        string
-	Pool             []string
-	DefaultLeaseTime time.Duration
-	MaxLeaseTime     time.Duration
+	Interface        string        `json:"interface"`
+	Pool             []string      `json:"pool"`
+	DefaultLeaseTime time.Duration `json:"default_lease_time"`
+	MaxLeaseTime     time.Duration `json:"max_lease_time"`
 
-	GatewayMode   Mode
-	Gateway       *string
-	DNSServerMode Mode
-	DNSServer     *[]string
-	NTPServerMode Mode
-	NTPServer     *[]string
+	GatewayMode   Mode      `json:"gateway_mode"`
+	Gateway       *string   `json:"gateway,omitempty"`
+	DNSServerMode Mode      `json:"dns_server_mode"`
+	DNSServers    *[]string `json:"dns_servers,omitempty"`
+	NTPServerMode Mode      `json:"ntp_server_mode"`
+	NTPServers    *[]string `json:"ntp_servers,omitempty"`
 
-	Reservations []Reservation
+	Reservations []Reservation `json:"reservations,omitempty"`
+
+	Comment string `json:"comment,omitempty"`
 }

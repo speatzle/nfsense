f5ea65010a92ebd9b7baf9ab9fe49d9cc1e4b8dc
Samuel Lorch <sam@soontm.de>
2023-05-13 21:03:32 +0200

    Remove Test Matcher

--- internal/definitions/firewall/match.go
+++ internal/definitions/firewall/match.go
@@ -1,7 +1,6 @@
 package firewall
 
 type Match struct {
-	TCPDestinationPort   uint64   `json:"tcp_destination_port,omitempty"`
 	Services             []string `json:"services,omitempty"`
 	SourceAddresses      []string `json:"source_addresses,omitempty"`
 	DestinationAddresses []string `json:"destination_addresses,omitempty"`

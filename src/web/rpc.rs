faf446d93a9cb243b2a5cfde121c08593b41c989
Samuel Lorch <sam@soontm.de>
2023-10-26 00:28:44 +0200

    Add temp api handle

--- src/web/rpc.rs
+++ src/web/rpc.rs
@@ -1,5 +1,26 @@
-use axum::Router;
+use super::super::AppState;
+use axum::routing::post;
+use axum::{Json, Router};
+use tower_cookies::{Cookie, Cookies};
+
+use axum::{
+    extract::Extension,
+    extract::State,
+    http::{Request, StatusCode},
+    middleware::{self, Next},
+    response::{IntoResponse, Response},
+};
+
+use tracing::info;
 
 pub fn routes() -> Router<super::super::AppState> {
-    Router::new()
+    Router::new().route("/api", post(api_handler))
+}
+
+async fn api_handler(
+    State(state): State<AppState>,
+    session: Extension<super::auth::Session>,
+) -> impl IntoResponse {
+    info!("api hit! user: {:?}", session.username);
+    StatusCode::NOT_IMPLEMENTED
 }

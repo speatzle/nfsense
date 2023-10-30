a9b99899c0271e53e942970351bf99b2c67d0d9d
Samuel Lorch <sam@soontm.de>
2023-10-30 22:11:17 +0100

    Replace duplicate get functions with macros

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,7 +6,18 @@ mod service;
 mod system;
 mod vpn;
 
-use crate::state::RpcState;
+use std::collections::HashMap;
+
+use crate::{
+    definitions::{
+        firewall::{DestinationNATRule, ForwardRule, SourceNATRule},
+        network::{NetworkInterface, StaticRoute},
+        object::{Address, Service},
+        service::{DHCPServer, DNSServer, NTPServer},
+        vpn::{WireguardInterface, WireguardPeer},
+    },
+    state::RpcState,
+};
 use jsonrpsee::{
     types::{error::ErrorCode, ErrorObject},
     RpcModule,
@@ -36,6 +47,7 @@ pub enum ApiError {
 
 impl Into<ErrorObject<'static>> for ApiError {
     fn into(self) -> ErrorObject<'static> {
+        info!("Converting Error {:?}", self);
         match self {
             // Todo Add Proper mappings
             // ApiError::ParameterDeserialize => ErrorCode::InvalidParams,
@@ -54,6 +66,56 @@ struct GetStringID {
 #[derive(Deserialize)]
 struct GetIntID {
     id: i64,
+}
+
+#[macro_export]
+macro_rules! get_map_thing {
+    ($( $sub_system:ident ).+) => {
+        |params, state| {
+            let t: GetStringID = params.parse().map_err(ApiError::ParameterDeserialize)?;
+
+            match state
+                .config_manager
+                .get_pending_config()
+                .$($sub_system).+
+                .get(&t.id)
+            {
+                Some(thing) => Ok(thing.clone()),
+                None => Err(ApiError::NotFound),
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! get_vec_thing {
+    ($( $sub_system:ident ).+) => {
+        |params, state| {
+            let t: GetIntID = params.parse().map_err(ApiError::ParameterDeserialize)?;
+            let things = state
+            .config_manager
+            .get_pending_config()
+            .$($sub_system).+;
+
+            if things.len() > t.id as usize{
+                Ok(things[t.id as usize].clone())
+            } else {
+                Err(ApiError::NotFound)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! get_things {
+    ($( $sub_system:ident ).+) => {
+        |_, state| {
+            Ok(state
+            .config_manager
+            .get_pending_config()
+            .$($sub_system).+)
+        }
+    };
 }
 
 pub fn new_rpc_module(state: RpcState) -> RpcModule<RpcState> {
@@ -80,48 +142,133 @@ pub fn new_rpc_module(state: RpcState) -> RpcModule<RpcState> {
         .unwrap();
 
     module
-        .register_method("network.get_static_routes", network::get_static_routes)
-        .unwrap();
-
-    module
-        .register_method("network.get_interfaces", network::get_interfaces)
-        .unwrap();
-
-    module
-        .register_method("object.get_services", object::get_services)
-        .unwrap();
-
-    module
-        .register_method("object.get_addresses", object::get_addresses)
-        .unwrap();
-
-    module
-        .register_method("service.get_dhcp_servers", service::get_dhcp_servers)
-        .unwrap();
-
-    module
-        .register_method("service.get_dns_servers", service::get_dns_servers)
-        .unwrap();
-
-    module
-        .register_method("service.get_ntp_servers", service::get_ntp_servers)
-        .unwrap();
-
-    module
-        .register_method("firewall.get_forward_rules", firewall::get_forward_rules)
-        .unwrap();
-
-    module
         .register_method(
-            "firewall.get_destination_nat_rules",
-            firewall::get_destination_nat_rules,
+            "network.get_static_route",
+            get_vec_thing!(network.static_routes),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<StaticRoute>, ApiError>, _>(
+            "network.get_static_routes",
+            get_things!(network.static_routes),
+        )
+        .unwrap();
+
+    module
+        .register_method("network.get_interface", get_map_thing!(network.interfaces))
+        .unwrap();
+
+    module
+        .register_method::<Result<HashMap<String, NetworkInterface>, ApiError>, _>(
+            "network.get_interfaces",
+            get_things!(network.interfaces),
+        )
+        .unwrap();
+
+    module
+        .register_method("object.get_service", get_map_thing!(object.services))
+        .unwrap();
+
+    module
+        .register_method::<Result<HashMap<String, Service>, ApiError>, _>(
+            "object.get_services",
+            get_things!(object.services),
+        )
+        .unwrap();
+
+    module
+        .register_method("object.get_address", get_map_thing!(object.addresses))
+        .unwrap();
+
+    module
+        .register_method::<Result<HashMap<String, Address>, ApiError>, _>(
+            "object.get_addresses",
+            get_things!(object.addresses),
         )
         .unwrap();
 
     module
         .register_method(
+            "service.get_dhcp_server",
+            get_vec_thing!(service.dhcp_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<DHCPServer>, ApiError>, _>(
+            "service.get_dhcp_servers",
+            get_things!(service.dhcp_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "service.get_dns_server",
+            get_vec_thing!(service.dns_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<DNSServer>, ApiError>, _>(
+            "service.get_dns_servers",
+            get_things!(service.dns_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "service.get_ntp_server",
+            get_vec_thing!(service.ntp_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<NTPServer>, ApiError>, _>(
+            "service.get_ntp_servers",
+            get_things!(service.ntp_servers),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "firewall.get_forward_rule",
+            get_vec_thing!(firewall.forward_rules),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<ForwardRule>, ApiError>, _>(
+            "firewall.get_forward_rules",
+            get_things!(firewall.forward_rules),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "firewall.get_destination_nat_rule",
+            get_vec_thing!(firewall.destination_nat_rules),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<DestinationNATRule>, ApiError>, _>(
+            "firewall.get_destination_nat_rules",
+            get_things!(firewall.destination_nat_rules),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "firewall.get_source_nat_rule",
+            get_vec_thing!(firewall.source_nat_rules),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<Vec<SourceNATRule>, ApiError>, _>(
             "firewall.get_source_nat_rules",
-            firewall::get_source_nat_rules,
+            get_things!(firewall.source_nat_rules),
         )
         .unwrap();
 
@@ -147,18 +294,35 @@ pub fn new_rpc_module(state: RpcState) -> RpcModule<RpcState> {
         .unwrap();
 
     module
-        .register_method("vpn.get_wireguard_status", vpn::get_wireguard_status)
+        .register_method("vpn.wireguard.get_status", vpn::get_wireguard_status)
         .unwrap();
 
     module
         .register_method(
-            "vpn.get_wireguard_interfaces",
-            vpn::get_wireguard_interfaces,
+            "vpn.wireguard.get_interface",
+            get_map_thing!(vpn.wireguard.interfaces),
         )
         .unwrap();
 
     module
-        .register_method("vpn.get_wireguard_peers", vpn::get_wireguard_peers)
+        .register_method::<Result<HashMap<String, WireguardInterface>, ApiError>, _>(
+            "vpn.wireguard.get_interfaces",
+            get_things!(vpn.wireguard.interfaces),
+        )
+        .unwrap();
+
+    module
+        .register_method(
+            "vpn.wireguard.get_peer",
+            get_map_thing!(vpn.wireguard.peers),
+        )
+        .unwrap();
+
+    module
+        .register_method::<Result<HashMap<String, WireguardPeer>, ApiError>, _>(
+            "vpn.wireguard.get_peers",
+            get_things!(vpn.wireguard.peers),
+        )
         .unwrap();
 
     module
--- src/api/object.rs
+++ src/api/object.rs
@@ -1,18 +1,1 @@
-use std::collections::HashMap;
 
-use jsonrpsee::types::Params;
-
-use crate::{
-    definitions::object::{Address, Service},
-    state::RpcState,
-};
-
-use super::ApiError;
-
-pub fn get_services(_: Params, state: &RpcState) -> Result<HashMap<String, Service>, ApiError> {
-    Ok(state.config_manager.get_pending_config().object.services)
-}
-
-pub fn get_addresses(_: Params, state: &RpcState) -> Result<HashMap<String, Address>, ApiError> {
-    Ok(state.config_manager.get_pending_config().object.addresses)
-}

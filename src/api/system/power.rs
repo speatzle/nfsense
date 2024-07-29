1e66729e5959bc33729be69c22e5942810ac65fc
Samuel Lorch <sam@soontm.de>
2024-07-29 23:49:50 +0200

    add system power api

---
+++ src/api/system/power.rs
@@ -0,0 +1,27 @@
+use crate::api::ApiError;
+use crate::state::RpcState;
+use jsonrpsee::types::Params;
+use jsonrpsee::RpcModule;
+use std::sync::Arc;
+
+pub fn register_methods(module: &mut RpcModule<RpcState>) {
+    module
+        .register_async_method("system.power.shutdown", halt_system)
+        .unwrap();
+
+    module
+        .register_async_method("system.power.restart", reboot_system)
+        .unwrap();
+}
+
+pub async fn halt_system<'a>(_: Params<'a>, _state: Arc<RpcState>) -> Result<(), ApiError> {
+    let systemd_manager = zbus_systemd::systemd1::ManagerProxy::new(&_state.dbus_conn).await?;
+    systemd_manager.halt().await?;
+    Ok(())
+}
+
+pub async fn reboot_system<'a>(_: Params<'a>, _state: Arc<RpcState>) -> Result<(), ApiError> {
+    let systemd_manager = zbus_systemd::systemd1::ManagerProxy::new(&_state.dbus_conn).await?;
+    systemd_manager.reboot().await?;
+    Ok(())
+}

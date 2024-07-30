3eb13e20d2a5cd5e975892537e896ca05681ebab
Samuel Lorch <sam@soontm.de>
2024-07-30 18:32:23 +0200

    Add Enum Type Display

---
+++ client/src/components/display/EnumTypeDisplay.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { variantOf } from '~/util';
+
+const props = withDefaults(defineProps<{
+  data: object | string,
+}>(), {
+  data: '',
+});
+</script>
+<template>
+  {{ variantOf(props.data) }}
+</template>

8e9d29327d04e55c58a8828f2ad23a7a15de276b
Samuel Lorch <sam@soontm.de>
2024-07-30 18:31:53 +0200

    Add Element Display

---
+++ client/src/components/display/ElementDisplay.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  data: any,
+  component?: string,
+  componentProp?: '',
+}>(), {
+  data: '',
+  component: '',
+  componentProp: '',
+});
+</script>
+<template>
+  <div v-if="data" class="pillbar">
+    <div class="pill">
+      <component v-bind="{[props.componentProp]: props.data}" :is="props.component" v-if="props.component !== ''"/>
+      <template v-else>{{ props.data }}</template>
+    </div>
+  </div>
+</template>

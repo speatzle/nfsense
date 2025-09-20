52c6361ed01388f5e5e57342fb8d7292f0cee43c
Samuel Lorch <sam@soontm.de>
2025-09-21 01:05:48 +0200

    Update install.sh to work with debian and newer nfsense versions

--- install.sh
+++ install.sh
@@ -3,64 +3,44 @@ echo "Installing nfsense dev env..."
 if [ "$(id -u)" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
 cd /root
 
-echo "Disable firewalld"
-systemctl stop firewalld
-
-echo "Disable SE Linux"
-setenforce 0
-sed -i s/^SELINUX=.*$/SELINUX=permissive/ /etc/selinux/config
-
-echo "Allow Routing"
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-sysctl -p
-
-echo "Setup Repos"
-dnf install epel-release -y
-/usr/bin/crb enable
-dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 
 echo "Installing Required Packages from Repos"
-dnf install wget git systemd-networkd gh tcpdump nodejs dhcp-server unbound wireguard-tools tar -y
+apt update
+DEBIAN_FRONTEND=noninteractive apt install tcpdump kea unbound chrony wireguard-tools tar ulogd2 ulogd2-json -y
+
+echo "Installing Required Dev Packages from Repos"
+DEBIAN_FRONTEND=noninteractive apt install curl git nodejs gcc pkg-config libssl-dev -y
 
 echo "Installing pnpm"
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 source /root/.bashrc
 
-echo "Installing go"
-wget "https://go.dev/dl/go1.21.2.linux-amd64.tar.gz"
-$ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.2.linux-amd64.tar.gz
-cat <<EOT >> /etc/profile
-export PATH=$PATH:/usr/local/go/bin
-EOT
-source /etc/profile
-
-echo "Installing code server"
-curl -fsSL https://code-server.dev/install.sh | sh
-mkdir -p /root/.config/code-server
-cat <<EOT >> /root/.config/code-server/config.yaml
-bind-addr: 0.0.0.0:8081
-auth: password
-password: nfsense
-cert: /root/cert.pem
-cert-key: /root/key.pem
-EOT
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
-systemctl enable --now code-server@root
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
 
 echo "Clone nfsense Repo"
-cd /opt
-git clone "https://github.com/speatzle/nfsense.git"
+# TODO test if this folder already exists
+if [ ! -d "/opt/nfsense" ]; then
+    cd /opt
+    git clone "https://github.com/speatzle/nfsense.git"
+else
+    echo "nfsense folder already exists"
+fi
 cd nfsense
 
-echo "build nfsense"
-go build
+echo "Generate SSL Certificates"
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
 
-echo "install client deps"
+echo "Build nfSense"
+cargo build
+
+echo "Install Client deps"
 cd client
 pnpm install
 cd ..
 
-echo "setup nfsense systemd services"
+echo "Setup nfSense systemd services"
 cat <<EOT >> /etc/systemd/system/nfsense.service
 [Unit]
 Description=nfsense api
@@ -70,7 +50,7 @@ After=network.target
 [Service]
 Type=simple
 WorkingDirectory=/opt/nfsense
-ExecStart=/opt/nfsense/nfsense
+ExecStart=/opt/nfsense/target/debug/nfsense
 Restart=on-failure
 RestartSec=10
 
@@ -95,30 +75,41 @@ RestartSec=10
 WantedBy=multi-user.target
 EOT
 
+mkdir /etc/nfsense
+
+touch /etc/chrony/conf.d/chrony-nfsense.conf
+
+unbound-control-setup
+
 echo "reload systemd"
 systemctl daemon-reload
 
 echo "Setup nftables"
-echo 'include "/etc/nftables/nfsense.conf"' >> /etc/sysconfig/nftables.conf
-touch /etc/nftables/nfsense.conf
+echo '#!/usr/sbin/nft -f\n' > /etc/nftables.conf
+echo 'include "/etc/nfsense/nfsense-nftables.conf"' >> /etc/nftables.conf
+touch /etc/nfsense/nfsense-nftables.conf
 
-echo "Setup nfsense Config"
-./nfsense setup
+echo "Disable ifupdown"
+mv /etc/network/interfaces /etc/network/interfaces.old
 
-# Enable & Disable Network Services
+echo "Allow Routing"
+echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/99-nfsense.conf
+systemctl restart systemd-sysctl
+
+echo "Generate nfsense Config"
+./target/debug/nfsense generate-default
+
+echo "Enable nftables"
 systemctl enable nftables
+
+echo "Enable systemd-networkd"
 systemctl enable systemd-networkd
-systemctl disable NetworkManager
-systemctl stop NetworkManager
-
-echo "Apply nfsense Config"
-./nfsense apply
-
 
 echo "Starting nfsense"
-systemctl enable nfsense
-systemctl start nfsense
-systemctl enable nfsense-client
-systemctl start nfsense-client
+systemctl enable --now nfsense
+systemctl enable --now nfsense-client
 
 echo "Done"
+echo "Note: you need to login to the web interface at https://<your-ip>:8080 with the user admin and password nfsense."
+echo "Configure the Network interfaces, your default route and apply the changes."
+echo "Otherwise there will be no network connectivity after reboot."

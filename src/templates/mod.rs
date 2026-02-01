e8297d46f69f3f1a2608ac67cafbe3d550124a2e
Samuel Lorch <sam@soontm.de>
2026-02-01 23:26:39 +0100

    Copy Templates into prod image This makes the image finally usable

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,8 +1,14 @@
 use tera::Tera;
 
+// TODO, better way to handle this?
+#[cfg(not(debug_assertions))]
+pub const TEMPLATE_PATH: &str = "/usr/share/nfsense/templates/**/*.*";
+#[cfg(debug_assertions)]
+pub const TEMPLATE_PATH: &str = "src/templates/**/*.*";
+
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("src/templates/**/*.*") {
+        let tera = match Tera::new(TEMPLATE_PATH) {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);

24245bd25d80f208689bd0a73cb23b263203311b
Samuel Lorch <sam@soontm.de>
2026-02-06 22:05:48 +0100

    Basic working imutable build

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-IMAGE_VERSION="$(git rev-parse --short=10 HEAD)-$(date --utc +%Y%m%d%H%M)"
+IMAGE_VERSION="git$(date --utc +%Y%m%d%H%M)-$(git rev-parse --short=10 HEAD)"
+#IMAGE_VERSION="v0.0.1"
 
-rm -f mkosi/mkosi.output/nfSense*
+rm -f mkosi/mkosi.output/nfsense* initrd*
 
 if [[ $# -ne 1 ]]; then
     echo "Prod Mode"
@@ -18,15 +19,26 @@ else
     fi
 fi
 
-RAW_FILE=$(find mkosi/mkosi.output -name "*${IMAGE_VERSION}.raw" -print -quit)
 
-if [ -z "$RAW_FILE" ]; then
-    echo "Error: Could not find raw image for version ${IMAGE_VERSION}"
+COMPRESSED_FILE=$(find mkosi/mkosi.output -name "nfsense_${IMAGE_VERSION}.raw.zst" -print -quit)
+
+if [ -z "$COMPRESSED_FILE" ]; then
+    echo "Error: Could not find image for version ${IMAGE_VERSION}"
     exit 1
 fi
+
+RAW_FILE="${COMPRESSED_FILE%.zst}"
+echo "Decompressing"
+zstd -d -f "$COMPRESSED_FILE" -o "$RAW_FILE"
+
 QCOW2_FILE="${RAW_FILE%.raw}.qcow2"
 
 
 echo "Converting to qcow2"
 qemu-img convert -p -f raw -O qcow2 -c "$RAW_FILE" "$QCOW2_FILE"
+qemu-img resize "$QCOW2_FILE" 32G
+
+echo "Calculate hash for qcow2"
+sha256sum "$QCOW2_FILE" >> "nfsense_${IMAGE_VERSION}.SHA256SUM"
+
 echo "Done"

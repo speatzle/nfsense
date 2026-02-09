1d0f367de2c2f54485bc3fb48ecf8b604da58a85
Samuel Lorch <sam@soontm.de>
2026-02-09 18:05:21 +0100

    Add Power Actions to Dashboard

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { apiCall } from '../api';
+import getPlugins from '../plugins';
+const p = getPlugins();
 
 type ServiceStatus = {
   name: string,
@@ -21,6 +23,24 @@ async function load(){
   loading = false;
 }
 
+async function restart(){
+  const res = await apiCall('system.power.restart', {});
+  if (res.Error === null) {
+    p.toast.success('Restart Triggered');
+  } else {
+    console.debug('error', res);
+  }
+}
+
+async function shutdown(){
+  const res = await apiCall('system.power.shutdown', {});
+  if (res.Error === null) {
+    p.toast.success('Shutdown Triggered');
+  } else {
+    console.debug('error', res);
+  }
+}
+
 onMounted(async() => {
   load();
 });
@@ -32,6 +52,8 @@ onMounted(async() => {
     <PageHeader title="Dashboard">
     </PageHeader>
     <template v-if="!loading">
+      <button @click="shutdown">Shutdown</button>
+      <button @click="restart">Restart</button>
       <div v-for="(status, index) in serviceStatus" :key="index">
         <p>{{ status.name }}: {{ status.status }}</p>
       </div>

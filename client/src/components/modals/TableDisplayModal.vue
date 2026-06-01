bbfa819a7b335cb3badcb674e3c85ec7a5c6c5da
adroslice <a.schlett@gmx.de>
2026-06-01 19:37:52 +0200

    feat: TableDisplayModal for showing KV-Pairs

---
+++ client/src/components/modals/TableDisplayModal.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{ data?: Record<string, any>; title?: string }>();
+</script>
+<template>
+  <Modal :title="props.title ?? 'Data'">
+    <table>
+      <thead>
+        <tr>
+          <th>Field</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="[key, value] of Object.entries(props.data ?? {})">
+          <td v-text="key" />
+          <td v-text="value" />
+        </tr>
+      </tbody>
+    </table>
+  </Modal>
+</template>
